a54f8e4254577b4a487730d760c6e1d1770ddff8
Mark Veltzer <mark.veltzer@gmail.com>
2014-10-20 15:11:51 +0300

    minor fixes

--- src/modules/github.pl
+++ src/modules/github.pl
@@ -6,15 +6,19 @@ Example of how to use a third party module, in this case the GitHub module.
 
 =cut
 
+# uses
+
 use Net::GitHub;
 use Config::IniFiles;
 use File::HomeDir;
+
+# code
 
 my($cfg)=Config::IniFiles->new( -file => File::HomeDir->my_home.'/.githubrc' ) or die('unable to access ini file');
 $param_login=$cfg->val('github', 'login');
 $param_pass=$cfg->val('github', 'pass');
 
-my $github=Net::GitHub->new(
+my($github)=Net::GitHub->new(
 	login => $param_login,
 	pass => $param_pass,
 );
