b578a12c09105248239ea61ea6a70773675c68fd
Mark Veltzer <mark.veltzer@gmail.com>
2015-11-07 08:39:55 +0200

    templar as optional

--- scripts/ubuntu_install.py
+++ scripts/ubuntu_install.py
@@ -7,17 +7,25 @@ ubuntu to compile and work with this package.
 
 import subprocess # for check_call
 
+do_mine=False
+
 packs=[
 	# perl
 	'libnet-github-perl',
 	'libconfig-inifiles-perl',
 	'libfile-homedir-perl',
 	'perl-tk',
-
-	# my own
-	'templar',
 ]
+
+if do_mine:
+	packs.extend([
+		# my own
+		'templar',
+	])
 
 args=['sudo','apt-get','install','--assume-yes']
 args.extend(packs)
-subprocess.check_call(args)
+try:
+	subprocess.check_call(args)
+except:
+	pass
