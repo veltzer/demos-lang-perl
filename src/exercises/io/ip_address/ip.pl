d33616c9f6c504fcf2f3cd10105cdb3ae53b5e2d
Mark Veltzer <mark.veltzer@gmail.com>
2016-05-15 12:03:35 +0300

    another exercise

---
+++ src/exercises/io/ip_address/ip.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+my @lines=`ifconfig`;
+foreach my $line (@lines) {
+	my $pos=index($line, 'inet addr:');
+	if($pos!=-1) {
+		my @sp=split /\s+/,$line;
+		#print join(',', @sp)."\n";
+		my $fip=$sp[2];
+		my $ip=substr($fip, 5);
+		print $ip."\n";
+	}
+}
