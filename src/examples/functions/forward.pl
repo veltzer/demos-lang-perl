39541387c0feaf9f1a92d5f70139915c839c5a9b
Mark Veltzer <mark.veltzer@gmail.com>
2016-05-17 13:23:41 +0300

    more examples

---
+++ src/examples/functions/forward.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w
+
+=head
+
+This is an example of forward functions in perl.
+
+=cut
+
+use strict;
+use warnings;
+
+sub one {
+	print "one\n";
+	two;
+}
+
+sub two {
+	print "two\n";
+}
+
+one
