5700c6005782575eed3a1c9453cae2e120253f42
Mark Veltzer <mark.veltzer@gmail.com>
2021-09-10 19:24:28 +0300

    fix config

--- config/project.py
+++ config/project.py
@@ -1,12 +1,12 @@
-'''
+"""
 project definitions for templar
-'''
+"""
 
 def populate(d):
-    d.project_name='demos-perl'
-    d.project_long_description='Demos for the Perl programming language'
-    d.project_year_started='2010'
-    d.project_description='TBD'
+    d.project_name="demos-perl"
+    d.project_long_description="Demos for the Perl programming language"
+    d.project_year_started="2010"
+    d.project_description="TBD"
 
 def getdeps():
     return [
