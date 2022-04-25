ba06aa2273c9f43bc0ebbc39886b624a811adabf
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-25 21:21:42 +0300

    fixing build

--- config/deps.py
+++ config/deps.py
@@ -1,16 +1,6 @@
-'''
-dependencies for this project
-'''
-
-def populate(d):
-    d.packs=[
-        'libnet-github-perl',
-        'libconfig-inifiles-perl',
-        'libfile-homedir-perl',
-        'perl-tk',
-    ]
-
-def getdeps():
-    return [
-        __file__, # myself
-    ]
+packs=[
+    'libnet-github-perl',
+    'libconfig-inifiles-perl',
+    'libfile-homedir-perl',
+    'perl-tk',
+]
