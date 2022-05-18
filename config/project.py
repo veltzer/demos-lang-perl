5cb9f707d36a09b9471e38f5ccd7cf04f77b7e55
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:50:57 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name="demos-perl"
-project_long_description="Demos for the Perl programming language"
-project_year_started=2010
-project_description="""TBD"""
+description_short="Demos for the Perl programming language"
